7959623c1794b27dc8bbb3aef8c84b354918bde5
joohee kim <dev.joohee0916@gmail.com>
2026-08-04 15:30:05 +0900

    fix: 중복 코드 삭제

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -19,9 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import com.example.demo.dto.comment.CreateCommentRequest;
 import com.example.demo.dto.comment.CreateCommentResponse;
 import com.example.demo.dto.comment.DeleteCommentResponse;
